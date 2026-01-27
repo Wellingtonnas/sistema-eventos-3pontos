b432f962717c2dff064733e6fde8fc46039475bc
Wellingtonnas <wellingtoncarneiro141@outlook.com>
2026-01-27 12:05:47 -0300

    Adiciona crédito no rodapé

---
+++ js/creditos.js
@@ -0,0 +1,7 @@
+// js/creditos.js
+(function () {
+  const footer = document.createElement("div");
+  footer.className = "footer-credito";
+  footer.textContent = "Desenvolvido por Wellington Carneiro";
+  document.body.appendChild(footer);
+})();
