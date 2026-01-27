c4b27f9d56932b05b65352144a28cec7457ea513
Wellingtonnas <wellingtoncarneiro141@outlook.com>
2026-01-27 12:23:43 -0300

    Adiciona crédito no rodapé

--- js/creditos.js
+++ js/creditos.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const footer = document.createElement("footer");
   footer.className = "footer-creditos";
-  footer.innerText = "Desenvolvidor Wellington Carneiro";
+  footer.innerText = "Desenvolvedor Wellington Carneiro";
 
   document.body.appendChild(footer);
 });
